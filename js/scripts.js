f409c32614467f393580661ebba1b21c87b438a1
Robert Lee <bobbydwise1@hotmail.com>
2018-11-02 10:01:54 -0700

    Add constructors to scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,33 @@
-function PizzaConstructor() {
+//Bussiness Logic
+function PizzaConstructor(pizzaSize, pizzaToppings, pizzaPriceFinal) {
   this.pizzaSize = pizzaSize,
   this.pizzaToppings = pizzaToppings,
   this.pizzaPriceFinal = pizzaPriceFinal
 }
 
+function PriceConstructor(singleItem, price) {
+  this.singleItem = singleItem,
+  this.price = price
+}
 
+var pizzaAvalibleSizes = ["small", "medium", "large"];
+var pizzaAvalibleSizesPrices = [10, 20, 30];
+
+function PizzaSizePricesArray(pizzaAvalibleSizes,pizzaAvalibleSizesPrices) {
+  for (i = 0; i < pizzaAvalibleSizes.length; i++) {
+    
+  }
+//var pizzaSizePrices = new PriceConstructor(pizzaAvalibleSizes, pizzaAvalibleSizesPrices);
+
+var pizzaAvalibleToppings = ["pineapples", "onions", "sasuage", "bacon", "pepperoni"];
+var pizzaAvalibleToppingsPrices = [1,2,3,4,5];
+//var pizzaToppingsPrices = new PriceConstructor(pizzaAvalibleToppings, pizzaAvalibleToppingsPrices);
+
+var pizza1 = new PizzaConstructor("string", "string", "string");
+console.log(pizza1);
+
+
+//UI Logic
 $(document).ready(function() {
 
 });
