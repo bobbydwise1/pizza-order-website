73577b990bb9adf018ec16bf75d9bcecc568244f
Robert Lee <bobbydwise1@hotmail.com>
2018-11-02 11:01:39 -0700

    Add test console.log scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,64 @@
 //Bussiness Logic
 function PizzaConstructor(pizzaSize, pizzaToppings, pizzaPriceFinal) {
   this.pizzaSize = pizzaSize,
-  this.pizzaToppings = pizzaToppings,
-  this.pizzaPriceFinal = pizzaPriceFinal
+  this.pizzaToppings = pizzaToppings
 }
 
-function PriceConstructor(singleItem, price) {
-  this.singleItem = singleItem,
-  this.price = price
+PizzaConstructor.prototype.PizzaPriceFinal = function(pizzaObject, pizzaPropertyArray, ojectArrayString, objectArrayPrice) {
+  var tempFinalPrice = 0;
+  pizzaObject.forEach()
 }
+
+function PriceConstructor(singleItem, price, id) {
+  this.singleItem = singleItem,
+  this.price = price,
+  this.id = id
+}
+
+function priceSummer(stringToSearch,priceArray) {
+  var tempPrice = priceArray.singleItem.search(stringToSearch);
+}
+
+//The below function creates the price array with an index
+function priceArrayCreate(arrayOfStringItems, arrayOfPrices) {
+var tempObjectPairs = [];
+  for (i = 0; i < arrayOfStringItems.length; i++) {
+    var tempObjectPair = new PriceConstructor(arrayOfStringItems[i],arrayOfPrices[i], i);
+    tempObjectPairs.push(tempObjectPair);
+  }
+  return tempObjectPairs;
+}
+
+function insertIndexToAnArrayOfObjects(arrayOfObjects) {
+  for (i = 0; i < arrayOfObjects.length; i++) {
+    arrayOfObjects[i].push(i);
+  }
+  return arrayOfObjects;
+}
+
+var userPizza1 = new PizzaConstructor("small", ["onions", "pineapples"]);
+var userPizza2 = new PizzaConstructor("small", ["pineapples"]);
+var userPizzasArray = [userPizza1,userPizza2];
+console.log(userPizzasArray);
 
 var pizzaAvalibleSizes = ["small", "medium", "large"];
 var pizzaAvalibleSizesPrices = [10, 20, 30];
-
-function PizzaSizePricesArray(pizzaAvalibleSizes,pizzaAvalibleSizesPrices) {
-  for (i = 0; i < pizzaAvalibleSizes.length; i++) {
-    
-  }
-//var pizzaSizePrices = new PriceConstructor(pizzaAvalibleSizes, pizzaAvalibleSizesPrices);
+var pizzaSizePriceObjectArray = priceArrayCreate(pizzaAvalibleSizes, pizzaAvalibleSizesPrices);
+console.log(pizzaSizePriceObjectArray);
 
 var pizzaAvalibleToppings = ["pineapples", "onions", "sasuage", "bacon", "pepperoni"];
 var pizzaAvalibleToppingsPrices = [1,2,3,4,5];
-//var pizzaToppingsPrices = new PriceConstructor(pizzaAvalibleToppings, pizzaAvalibleToppingsPrices);
+var pizzaToppingsPriceObjectArray = priceArrayCreate(pizzaAvalibleToppings, pizzaAvalibleToppingsPrices);
+console.log(pizzaToppingsPriceObjectArray);
 
-var pizza1 = new PizzaConstructor("string", "string", "string");
-console.log(pizza1);
+
+
+
+
+
+
+
+
 
 
 //UI Logic
