da2bdef83acb808127e3a62dfce833f7ef8aa68a
Robert Lee <bobbydwise1@hotmail.com>
2018-11-02 14:14:29 -0700

    delete all price constructors

--- js/scripts.js
+++ js/scripts.js
@@ -19,77 +19,41 @@ function PizzaConstructor(pizzaSize, pizzaToppings, pizzaPriceFinal) {
 }
 
 //Logic for determining the intial price of a single pizza object
-PizzaConstructor.prototype.PizzaPriceFinal = function(pizzaSizePriceObjectArray, pizzaToppingsPriceObjectArray) {
+PizzaConstructor.prototype.PizzaPriceFinal = function() {
   var tempFinalPrice = 0;
-  var tempSize = this.pizzaSize;
-  var tempToppings = this.pizzaToppings;
-  console.log(tempFinalPrice, tempSize, tempToppings);
-  for (i = 0; i < Object.keys(pizzaSizePriceObjectArray).length; i++)
-  if (tempSize === pizzaSizePriceObjectArray.) {
-    return
-  }
-
-}
-
-//Logic for creating an empty prices object
-function pricesArray() {
-  this.priceListing = [],
-  this.id = -1
-}
-//Logic for creating methods for the pricesArray object
-pricesArray.prototype.addNewPrice = function(priceListing) {
-  priceListing.id = this.assignId();
-  this.priceListing.push(priceListing);
-}
-pricesArray.prototype.assignId = function() {
-  this.id += 1;
-  return this.id;
-}
-pricesArray.prototype.findAPriceForThis = function (someString) {
-  console.log(this.length);
-  for (i = 0; i < this.length; i++) {
-
+  if (this.pizzaSize === "small") {
+    tempFinalPrice += 10;
+  } else if (this.pizzaSize === "medium") {
+    tempFinalPrice += 20;
+  } else if (this.pizzaSize === "large") {
+    tempFinalPrice += 30;
+  } else {}
+  console.log("Pan Price price: " + tempFinalPrice);
+  for (i = 0; i < this.pizzaToppings.length; i++) {
+    if (this.pizzaToppings[i] === "sausage") {
+      tempFinalPrice += 5;
+    } else if (this.pizzaToppings[i] === "bacon") {
+      tempFinalPrice += 4;
+    } else if (this.pizzaToppings[i] === "pepperoni") {
+      tempFinalPrice += 3;
+    } else if (this.pizzaToppings[i] === "pineapples") {
+      tempFinalPrice += 2;
+    } else if (this.pizzaToppings[i] === "onions") {
+      tempFinalPrice += 1;
+    } else {}
+    console.log("Pan Price price: " + tempFinalPrice);
+    return tempFinalPrice;
   }
 }
-
-//Logic for the price constructor
-function PriceConstructor(thingToBuy, price) {
-  this.thingToBuy = thingToBuy,
-  this.price = price,
-  this.id = 0
-}
-
-//The below function creates the price array with an index
-function priceArrayCreate(arrayOfStringItems, arrayOfPrices) {
-  var tempObjectPairs = new pricesArray();
-//console.log(tempObjectPairs);
-  for (i = 0; i < arrayOfStringItems.length; i++) {
-    var tempObjectPair = new PriceConstructor(arrayOfStringItems[i],arrayOfPrices[i]);
-    tempObjectPairs.addNewPrice(tempObjectPair);
-  }
-  return tempObjectPairs;
-}
-
 //Test Database of user's pizzas
-var userPizza1 = new PizzaConstructor("small", ["onions", "pineapples"]);
-var userPizza2 = new PizzaConstructor("medium", ["pineapples"]);
-var userPizza3 = new PizzaConstructor("large", ["sasuage", "bacon", "pepperoni"])
-var usersNewListofPizzas = new UserPizzasArray();
-usersNewListofPizzas.addNewPizza(userPizza1);
-usersNewListofPizzas.addNewPizza(userPizza2);
-usersNewListofPizzas.addNewPizza(userPizza3);
-console.log(usersNewListofPizzas);
-//Test Database of pizza size prices
-var pizzaAvalibleSizes = ["small", "medium", "large"];
-var pizzaAvalibleSizesPrices = [10, 20, 30];
-var pizzaSizePriceObjectArray = priceArrayCreate(pizzaAvalibleSizes, pizzaAvalibleSizesPrices);
-console.log(pizzaSizePriceObjectArray);
-//Test Database of pizza toppings prices
-var pizzaAvalibleToppings = ["pineapples", "onions", "sasauge", "bacon", "pepperoni"];
-var pizzaAvalibleToppingsPrices = [1,2,3,4,5];
-var pizzaToppingsPriceObjectArray = priceArrayCreate(pizzaAvalibleToppings, pizzaAvalibleToppingsPrices);
-console.log(pizzaToppingsPriceObjectArray);
-
+var pizza1 = new PizzaConstructor("small", ["onions", "pineapples"]);
+var pizza2 = new PizzaConstructor("medium", ["pineapples"]);
+var pizza3 = new PizzaConstructor("large", ["sausage", "bacon", "pepperoni", "pineapples", "onions"])
+var pizzas = new UserPizzasArray();
+pizzas.addNewPizza(pizza1);
+pizzas.addNewPizza(pizza2);
+pizzas.addNewPizza(pizza3);
+console.log(pizzas);
 
 
 
